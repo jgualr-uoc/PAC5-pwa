7405c75c5211e1633ae9a193a89012ccc23e17a7
JosepGual <josep.gualr@gmail.com>
2025-05-30 19:31:08 +0200

    components and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './Components/home/home';
+import { Detail } from './Components/detail/detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'detail/:id', component: Detail},
+    {path: '**', redirectTo: '', pathMatch: 'full'}
+];
